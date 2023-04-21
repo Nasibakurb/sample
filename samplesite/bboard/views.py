c67254daa2837949dae41358dd27b2e51fbb749b
КурбанджановаН <nasibaukr@mail.ru>
2023-04-21 17:32:23 +0600

    all

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,3 +1,22 @@
 from django.http import HttpResponse
+from bboard.models import Bb
+from django.template import loader
+from django.shortcuts import render
+
 def index(request):
-    return HttpResponse("Здесь будет выведен список .")
+    bbs = Bb.objects.order_by('-published')
+    return render(request, 'bboard/index.html', {'bbs': bbs})
+
+    # template = loader.get_template('bboard/index.html')
+    # bbs = Bb.objects.order_by('-published')
+    # context = {'bbs': bbs}
+    # return HttpResponse(template.render(context, request))
+
+    # s = "Список объявление \r\r\r\r"
+    # for bb in Bb.objects.order_by('-published'):
+    #     s += bb.title + '\r\n' + bb.content + '\r\n\r\n'
+    # return HttpResponse(s, content_type='text/plain; charset=utf-8')
+
+
+
+
